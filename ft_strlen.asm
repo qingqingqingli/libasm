2851ec49b9241a01610e1a67c63c49389b9189a2
qingqingqingli <kadlqbetty@gmail.com>
2021-02-01 17:03:27 +0100

    update ft_strlen

--- ft_strlen.asm
+++ ft_strlen.asm
@@ -1,14 +1,14 @@
 global	_start		; entry point for the program
 
 section .data
-	msg db "test", 0x0a
-	len equ $ - msg
+	hello db "Welcome back", 0x0a
+	len equ $ - hello
 
 section .text
 _start:
 	mov rax, 4
 	mov rbx, 1
-	mov rcx, msg
+	mov rcx, hello
 	mov rdx, len
 	int 0x80
 	mov rax, 1
