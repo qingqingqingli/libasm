1bcf92febe11b5f15acecd03a8d360a1def38940
qingqingqingli <kadlqbetty@gmail.com>
2021-02-02 16:56:48 +0100

    add practice file

--- ft_strlen.asm
+++ ft_strlen.asm
@@ -1,13 +1,17 @@
+section 	.text
 global		ft_strlen		; entry point for the program
 
-section 	.text
-
 ft_strlen:
-	mov		rcx, -1
-	mov 	rsi, rdi		; back up rdi
-	mov 	rax, 0			; look for \0
-	repne	scasb			; do the search
-	sub 	rdi, rsi		; save the srting length
-	dec 	rdi				; decrement one for \0
-	mov		rax, rdi		; save the return value
-	ret
+			mov		rcx, -1
+			mov 	rsi, rdi		; back up rdi
+			mov 	rax, 0			; look for \0
+			repne	scasb			; do the search
+			sub 	rdi, rsi		; save the srting length
+			dec 	rdi				; decrement one for \0
+			mov		rax, rdi		; save the return value
+			ret
+
+; -> produce a .o file, which needs to be linked to become an executable
+
+; nasm -f elf64 -o hello.o hello.asm
+; ld hello.o -o hello
---
+++ x86_64 examples/user_input.asm
@@ -0,0 +1,56 @@
+section .data
+	text1 db "what is your name?"
+	text2 db "Hello, "
+
+section .bss
+	name resb 16	; reserve 16 bytes
+
+section .text
+	global _start
+
+_start:
+
+	call _printText1
+	call _getName
+	call _printText2
+	call _printName
+
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+_getName:
+	mov	rax, 0		; the id number of syscall sys_read
+	mov rdi, 0		; read from stdin
+	mov rsi, name	; save what's read into rsi
+	mov rdx, 16
+	syscall
+	ret
+
+_printText1:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text1
+	mov rdx, 18
+	syscall
+	ret
+
+_printText2:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text2
+	mov rdx, 7
+	syscall
+	ret
+
+_printName:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, name
+	mov rdx, 16			; max length of the name
+	syscall
+	ret
+
+; nasm -felf64 user_input.asm && ld user_input.o && ./a.out
+; rm *.o *.out
