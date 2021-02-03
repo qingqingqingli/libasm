613ed7fbcafef16368453a21fac677d698fe78e7
qingqingqingli <kadlqbetty@gmail.com>
2021-02-03 15:24:26 +0100

    update ft_strcpy

---
+++ ft_strcpy.asm
@@ -0,0 +1,31 @@
+
+; -------------------------------------------
+; [strcpy]
+; - copy a string
+; - char *strcpy(char *dest, const char *src);
+; - The strcpy() function copies the string pointed to by src, including the
+;   terminating '\0', to the buffer pointed to by dest. Strings may not overlap.
+;   The destination string dest must be large enough to receive the copy.
+; - The strcpy() return a pointer to the destination string dest.
+
+; [Registers]
+; rdi -> char *dest
+; rsi -> char *src
+; rax -> return value
+; -------------------------------------------
+
+section		.text
+global		ft_strcpy
+
+ft_strcpy:
+	mov		rax, 0					; initialise rax to 0
+	mov 	rcx, 0					; initialise counter to 0
+
+_loop:
+	mov		al, byte[rsi + rcx]		; save src char into al
+	mov		[rdi + rcx], al			; save al into dest char
+	inc		rcx						; increment counter
+	cmp		al, 0					; check if it's the end
+	jne		_loop					; start the loop again when it's not the end
+
+	ret								; return after it reaches the end
--- main.c
+++ main.c
@@ -1,23 +1,59 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#define SIZE 200
 
-extern size_t ft_strlen(const char *s);
+size_t ft_strlen(const char *s);
+char *ft_strcpy(char *dest, const char *src);
 
 int main(void)
 {
 	char *string1 = "123456789";
 	char *string2 = "asdfasdfasdfasdf";
 	char *string3 = "";
-	char *string4 = "------------------9";
+	char *string4 = "------------------9------------------sdfsfsdfsfasdfasdf---------------asdfasdf";
 	char *string5 = NULL;
 
-	ft_strlen(string1);
-	printf("original is [%ld] \t test is [%ld]\n", strlen(string1), ft_strlen(string1));
-	printf("original is [%ld] \t test is [%ld]\n", strlen(string2), ft_strlen(string2));
-	printf("original is [%ld] \t test is [%ld]\n", strlen(string3), ft_strlen(string3));
-	printf("original is [%ld] \t test is [%ld]\n", strlen(string4), ft_strlen(string4));
-	//printf("original is [%ld] \t test is [%ld]\n", strlen(string5), ft_strlen(string5)); // will seg
+	// printf("--------------ft_strlen---------------\n");
+	// printf("original is [%ld] \t test is [%ld]\n", strlen(string1), ft_strlen(string1));
+	// printf("original is [%ld] \t test is [%ld]\n", strlen(string2), ft_strlen(string2));
+	// printf("original is [%ld] \t test is [%ld]\n", strlen(string3), ft_strlen(string3));
+	// printf("original is [%ld] \t test is [%ld]\n", strlen(string4), ft_strlen(string4));
+	// //printf("original is [%ld] \t test is [%ld]\n", strlen(string5), ft_strlen(string5)); // will seg
+
+	printf("--------------ft_strcpy---------------\n");
+
+	char *dest = (char *)malloc(sizeof(char) * SIZE);
+	// strcpy(dest, string1);
+	// printf("original is \t [%s]\n", dest);
+	ft_strcpy(dest, string1);
+	printf("copy is \t [%s]\n", dest);
+	printf("[%c]\n", string1[9]);
+	printf("[%c]\n", dest[9]);
+
+	// strcpy(dest, string2);
+	// printf("original is \t [%s]\n", dest);
+	ft_strcpy(dest, string2);
+	printf("copy is \t [%s]\n", dest);
+
+	// strcpy(dest, string3);
+	// printf("original is \t [%s]\n", dest);
+	ft_strcpy(dest, string3);
+	printf("copy is \t [%s]\n", dest);
+
+	// strcpy(dest, string4);
+	// printf("original is \t [%s]\n", dest);
+	ft_strcpy(dest, string4);
+	printf("copy is \t [%s]\n", dest);
+
+	// strcpy(dest, string5);
+	// printf("original is \t [%s]\n", dest);
+	// ft_strcpy(dest, string5);
+	// printf("copy is \t [%s]\n", dest);
+
+	free(dest);
 	return 0;
 }
 
 // nasm -felf64 ft_strlen.asm && gcc main.c ft_strlen.o && ./a.out
+// nasm -felf64 ft_strcpy.asm && gcc main.c ft_strcpy.o && ./a.out
