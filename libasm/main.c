f6a8eb8d623dfce20b74e27c1465ca85e5237418
qingqingqingli <kadlqbetty@gmail.com>
2021-02-05 12:19:25 +0100

    udpate test amin

--- libasm/main.c
+++ libasm/main.c
@@ -2,17 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <errno.h>
 #include <string.h>
-
-#define Black "\033[0;30m"
-#define Red "\033[0;31m"
-#define Green "\033[0;32m"
-#define Yellow "\033[0;33m"
-#define Blue "\033[0;34m"
-#define Purple "\033[0;35m"
-#define Cyan "\033[0;36m"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 extern int errno;
 
@@ -23,180 +17,174 @@ ssize_t ft_read(int fd, void *buf, size_t count);
 ssize_t ft_write(int fd, const void *buf, size_t count);
 char *ft_strdup(const char *s);
 
-// void	strlen_test()
-// {
+void	strlen_test()
+{
 
-// 	printf("--------------ft_strlen---------------\n");
+	printf("--------------ft_strlen---------------\n\n");
 
-// 	char *str1 = "123456789";
-// 	char *str2 = "asdfasdfasdfasdf";
-// 	char *str3 = "";
-// 	char *str4 = "------------------9------------------sdfsfsdfsfasdfasdf---------------asdfasdf";
-// 	char *str5 = NULL;
+	char *str1 = "123456789";
+	char *str2 = "asdfasdfasdfasdf";
+	char *str3 = "";
+	char *str4 = "------------------9------------------sdfsfsdfsfasdfasdf---------------asdfasdf";
+	char *str5 = NULL;
 
-// 	printf("Original is \t [%ld] \ntest is \t [%ld]\n", strlen(str1), ft_strlen(str1));
-// 	printf("Original is \t [%ld] \ntest is \t [%ld]\n", strlen(str2), ft_strlen(str2));
-// 	printf("Original is \t [%ld] \ntest is \t [%ld]\n", strlen(str3), ft_strlen(str3));
-// 	printf("Original is \t [%ld] \ntest is \t [%ld]\n", strlen(str4), ft_strlen(str4));
-// 	//printf("Original is \t [%ld] \ntest is \t [%ld]\n", strlen(str5), ft_strlen(str5)); // will seg
+	printf("strlen is \t [%ld] \nft_strlen is \t [%ld]\n\n", strlen(str1), ft_strlen(str1));
+	printf("strlen is \t [%ld] \nft_strlen is \t [%ld]\n\n", strlen(str2), ft_strlen(str2));
+	printf("strlen is \t [%ld] \nft_strlen is \t [%ld]\n\n", strlen(str3), ft_strlen(str3));
+	printf("strlen is \t [%ld] \nft_strlen is \t [%ld]\n\n", strlen(str4), ft_strlen(str4));
+	//printf("strlen is \t [%ld] \nft_strlen is \t [%ld]\n\n", strlen(str5), ft_strlen(str5)); // will seg
 
-// }
+}
 
-// int		strcpy_test()
-// {
-// 	printf("--------------ft_strcpy---------------\n");
+int		strcpy_test()
+{
+	printf("\n--------------ft_strcpy---------------\n\n");
 
-// 	char	dst_one[250];
-// 	char	dst_two[250];
+	char	dst_one[250];
+	char	dst_two[250];
 
-// 	char *str1 = "123456789";
-// 	char *str2 = "asdfasdfasdfasdf";
-// 	char *str3 = "";
-// 	char *str4 = "------------------9------------------sdfsfsdfsfasdfasdf---------------asdfasdf";
-// 	char *str5 = "test2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatie";
-// 	char *str6 = NULL;
+	char *str1 = "123456789";
+	char *str2 = "asdfasdfasdfasdf";
+	char *str3 = "";
+	char *str4 = "------------------9------------------sdfsfsdfsfasdfasdf---------------asdfasdf";
+	char *str5 = "this is a very looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong string to test a function";
+	char *str6 = NULL;
 
-// 	printf("Original is \t [%s] \ntest is \t [%s]\n", strcpy(dst_two, str1), ft_strcpy(dst_two, str1));
-// 	printf("Original is \t [%s] \ntest is \t [%s]\n", strcpy(dst_two, str2), ft_strcpy(dst_two, str2));
-// 	printf("Original is \t [%s] \ntest is \t [%s]\n", strcpy(dst_two, str3), ft_strcpy(dst_two, str3));
-// 	printf("Original is \t [%s] \ntest is \t [%s]\n", strcpy(dst_two, str4), ft_strcpy(dst_two, str4));
-// 	printf("Original is \t [%s] \ntest is \t [%s]\n", strcpy(dst_two, str5), ft_strcpy(dst_two, str5));
-// 	// printf("Original is \t [%s] \ntest is \t [%s]\n", strcpy(dst_two, str6), ft_strcpy(dst_two, str6));
-// }
+	printf("strcpy is \t [%s] \nft_strcpy is \t [%s]\n\n", strcpy(dst_two, str1), ft_strcpy(dst_two, str1));
+	printf("strcpy is \t [%s] \nft_strcpy is \t [%s]\n\n", strcpy(dst_two, str2), ft_strcpy(dst_two, str2));
+	printf("strcpy is \t [%s] \nft_strcpy is \t [%s]\n\n", strcpy(dst_two, str3), ft_strcpy(dst_two, str3));
+	printf("strcpy is \t [%s] \nft_strcpy is \t [%s]\n\n", strcpy(dst_two, str4), ft_strcpy(dst_two, str4));
+	printf("strcpy is \t [%s] \nft_strcpy is \t [%s]\n\n", strcpy(dst_two, str5), ft_strcpy(dst_two, str5));
+	// printf("strcpy is \t [%s] \nft_strcpy is \t [%s]\n\n", strcpy(dst_two, str6), ft_strcpy(dst_two, str6));
+}
 
-// void	strcmp_test()
-// {
-// 	// printf("--------------ft_strcmp---------------\n");
+void	strcmp_test()
+{
+	printf("\n--------------ft_strcmp---------------\n\n");
 
-// 	char *str1 = "6666123456789";
-// 	char *str2 = "6666asdfasdfasdfasdf";
-// 	char *str3 = "";
-// 	char *str4 = "------------------9------------------sdfsfsdfsfasdfasdf---------------asdfasdf";
-// 	char *str5 = NULL;
+	char *str1 = "6666123456789";
+	char *str2 = "6666asdfasdfasdfasdf";
+	char *str3 = "";
+	char *str4 = "------------------9------------------sdfsfsdfsfasdfasdf---------------asdfasdf";
+	char *str5 = NULL;
 
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str1, str2), ft_strcmp(str1, str2));
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str2, str3), ft_strcmp(str2, str3));
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str3, str4), ft_strcmp(str3, str4));
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str4, str3), ft_strcmp(str4, str3));
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str3, str2), ft_strcmp(str3, str2));
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str2, str1), ft_strcmp(str2, str1));
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str1, str1), ft_strcmp(str1, str1));
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str2, str2), ft_strcmp(str2, str2));
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str3, str3), ft_strcmp(str3, str3));
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str4, str4), ft_strcmp(str4, str4));
-// 	printf("Original is \t [%d] \t custom is \t [%d]\n", strcmp(str5, str4), ft_strcmp(str5, str4));
-// }
+	printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str1, str2), ft_strcmp(str1, str2));
+	printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str2, str3), ft_strcmp(str2, str3));
+	printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str3, str4), ft_strcmp(str3, str4));
+	printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str4, str3), ft_strcmp(str4, str3));
+	printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str3, str2), ft_strcmp(str3, str2));
+	printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str2, str1), ft_strcmp(str2, str1));
+	printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str1, str1), ft_strcmp(str1, str1));
+	printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str2, str2), ft_strcmp(str2, str2));
+	printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str3, str3), ft_strcmp(str3, str3));
+	printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str4, str4), ft_strcmp(str4, str4));
+	// printf("strcmp is \t [%d] \t ft_strcmp is \t [%d]\n", strcmp(str5, str4), ft_strcmp(str5, str4));
+}
 
-// void 	read_test(void)
-// {
-// 	printf("--------------ft_read---------------\n\n");
+void 	read_test(void)
+{
+	printf("\n--------------ft_read---------------\n\n");
 
-// 	char *file1 = "main.c";
-// 	char *file2 = "";
-// 	char *buf1 = malloc(sizeof(char) * 50);
-// 	char *buf2 = malloc(sizeof(char) * 50);
-// 	int fd1 = open(file1, O_RDONLY);
-// 	int	fd2 = open(file2, O_RDONLY);
+	char *file1 = "test.txt";
+	char *file2 = "";
+	char *buf1 = malloc(sizeof(char) * 50);
+	char *buf2 = malloc(sizeof(char) * 50);
+	int fd1 = open(file1, O_RDONLY);
+	int	fd2 = open(file2, O_RDONLY);
 
-// 	errno = 0;
-// 	printf(Blue "Original return value \t [%zd]\n", read(fd1, buf1, 40));
-// 	printf("Original read \t\t [%s]\n", buf1);
-// 	printf("Original errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	errno = 0;
-// 	printf(Purple "copy return value \t [%zd]\n", ft_read(fd1, buf1, 40));
-// 	printf("copy read \t\t [%s]\n", buf1);
-// 	printf("copy errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	printf("\n");
+	errno = 0;
+	printf("read() return value \t [%zd]\n", read(fd1, buf1, 40));
+	printf("read() read buf \t [%s]\n", buf1);
+	printf("read() errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
+	errno = 0;
+	printf("ft_read() return value \t [%zd]\n", ft_read(fd1, buf1, 40));
+	printf("ft_read() read buf \t [%s]\n", buf1);
+	printf("ft_read() errno \t [%d] \t [%s]\n", errno, strerror(errno));
+	printf("\n");
 
-// 	errno = 0;
-// 	printf(Blue "Original return value \t [%zd]\n", read(fd2, buf2, 40));
-// 	printf("Original read \t\t [%s]\n", buf2);
-// 	printf("Original errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	errno = 0;
-// 	printf(Purple "copy return value \t [%zd]\n", ft_read(fd2, buf2, 40));
-// 	printf("copy read \t\t [%s]\n", buf2);
-// 	printf("copy errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	close(fd2);
-// 	printf("\n");
+	errno = 0;
+	printf("read() return value \t [%zd]\n", read(fd2, buf2, 40));
+	printf("read() read buf \t [%s]\n", buf2);
+	printf("read() errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
+	errno = 0;
+	printf("ft_read() return value \t [%zd]\n", ft_read(fd2, buf2, 40));
+	printf("ft_read() read buf \t [%s]\n", buf2);
+	printf("ft_read() errno \t [%d] \t [%s]\n", errno, strerror(errno));
+	close(fd2);
+	printf("\n");
 
-// 	errno = 0;
-// 	printf(Blue "Original return value \t [%zd]\n", read(fd1, 0, 40));
-// 	printf("Original errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	errno = 0;
-// 	printf(Purple "copy return value \t [%zd]\n", ft_read(fd1, 0, 40));
-// 	printf("copy errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	close(fd1);
-// 	printf("\n");
+	errno = 0;
+	printf("read() return value \t [%zd]\n", read(fd1, 0, 40));
+	printf("read() errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
+	errno = 0;
+	printf("ft_read() return value \t [%zd]\n", ft_read(fd1, 0, 40));
+	printf("ft_read() errno \t [%d] \t [%s]\n", errno, strerror(errno));
+	close(fd1);
+	printf("\n");
 
-// 	errno = 0;
-// 	printf(Blue "Original return value \t [%zd]\n", read(0, buf1, 40));
-// 	printf("Original errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	errno = 0;
-// 	printf(Purple "copy return value \t [%zd]\n", ft_read(0, buf1, 40));
-// 	printf("copy errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	printf("\n");
+	// errno = 0;
+	// printf("read() return value \t [%zd]\n", read(0, buf1, 40));
+	// printf("read() errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
+	// errno = 0;
+	// printf("ft_read() return value \t [%zd]\n", ft_read(0, buf1, 40));
+	// printf("ft_read() errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
+	// printf("\n");
 
-// 	free(buf1);
-// 	free(buf2);
-// }
+	free(buf1);
+	free(buf2);
+}
 
-// void	write_test()
-// {
-// 	printf("--------------ft_write---------------\n\n");
-// 	char *buf = "TEST SENTENCE TO TEST WRITE.\n";
-// 	int fd = 1;
-// 	int n = strlen(buf);
-// 	errno = 0;
-// 	printf("Original return value \t [%zd]\n", write(fd, buf, n));
-// 	printf("Copy return value \t [%zd]\n", ft_write(fd, buf, n));
-// 	printf("\n");
-// 	errno = 0;
-// 	printf(Blue "Original return value \t [%zd]\n", write(fd, 0, n));
-// 	printf("Original errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	printf("\n");
-// 	errno = 0;
-// 	printf(Yellow "Copy return value \t [%zd]\n", ft_write(fd, 0, n));
-// 	printf("copy errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	printf("\n");
-// 	errno = 0;
-// 	printf(Blue "Original return value \t [%zd]\n", write(-1, 0, n));
-// 	printf("Original errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	printf("\n");
-// 	errno = 0;
-// 	printf(Yellow "Copy return value \t [%zd]\n", ft_write(-1, 0, n));
-// 	printf("Copy errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
-// 	printf("\n");
-// }
+void	write_test()
+{
+	printf("\n--------------ft_write---------------\n\n");
+	char *buf = "TEST SENTENCE TO TEST WRITE.\n";
+	int fd = 1;
+	int n = strlen(buf);
+	errno = 0;
+	printf("write() return value \t\t [%zd]\n", write(fd, buf, n));
+	printf("ft_write() return value \t [%zd]\n", ft_write(fd, buf, n));
+	printf("\n");
+	errno = 0;
+	printf("write() return value \t\t [%zd]\n", write(fd, 0, n));
+	printf("write() errno \t\t\t [%d] \t [%s]\n", errno, strerror(errno));
+	printf("\n");
+	errno = 0;
+	printf("ft_write() return value \t [%zd]\n", ft_write(fd, 0, n));
+	printf("ft_write() errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
+	printf("\n");
+	errno = 0;
+	printf("write() return value \t\t [%zd]\n", write(-1, 0, n));
+	printf("write() errno \t\t\t [%d] \t [%s]\n", errno, strerror(errno));
+	printf("\n");
+	errno = 0;
+	printf("ft_write() return value \t [%zd]\n", ft_write(-1, 0, n));
+	printf("ft_write() errno \t\t [%d] \t [%s]\n", errno, strerror(errno));
+	printf("\n");
+}
 
 void	ft_strdup_test(void)
 {
-	printf(Blue "strdup \t\t [%s]\n", strdup("test1"));
-	printf(Yellow "ft_strdup \t [%s]\n", ft_strdup("test1"));
+	printf("\n--------------ft_strdup---------------\n\n");
+	printf("strdup \t\t [%s]\n", strdup("test1"));
+	printf("ft_strdup \t [%s]\n", ft_strdup("test1"));
 	printf("\n\n");
-	printf(Blue "strdup \t\t [%s]\n", strdup("test2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatie"));
-	printf(Yellow "ft_strdup \t [%s]\n", ft_strdup("test2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatietest2 met spatie"));
+	printf("strdup \t\t [%s]\n", strdup("this is a very looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong string to test a function"));
+	printf("ft_strdup \t [%s]\n", ft_strdup("this is a very looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong string to test a function"));
 	printf("\n\n");
-	printf(Blue "strdup \t\t [%s]\n", strdup(""));
-	printf(Yellow "ft_strdup \t [%s]\n", ft_strdup(""));
+	printf("strdup \t\t [%s]\n", strdup(""));
+	printf("ft_strdup \t [%s]\n", ft_strdup(""));
 }
 
 int 	main(void)
 {
-	// strlen_test();
-	// strcpy_test();
-	// strcmp_test();
-	// read_test();
-	// write_test();
+	strlen_test();
+	strcpy_test();
+	strcmp_test();
+	read_test();
+	write_test();
 	ft_strdup_test();
 	return 0;
 }
 
-// nasm -felf64 ft_strlen.asm && gcc main.c ft_strlen.o && ./a.out
-// nasm -felf64 ft_strcpy.asm && gcc main.c ft_strcpy.o && ./a.out
-// nasm -felf64 ft_strcmp.asm && gcc main.c ft_strcmp.o && ./a.out
-// nasm -felf64 ft_read.asm && gcc -no-pie main.c ft_read.o && ./a.out
-// nasm -felf64 ft_write.asm && gcc -no-pie main.c ft_write.o && ./a.out
-// nasm -felf64 ft_strdup.asm && gcc -no-pie main.c ft_strdup.o && ./a.out
-// rm *.o *.out
-
-// nasm -felf64 ft_strdup.asm && gcc -no-pie main.c ft_strlen.o ft_strcpy.o ft_strdup.o && ./a.out
+// Compile on Linux
+// gcc -no-pie main.c libasm.a && ./a.out
