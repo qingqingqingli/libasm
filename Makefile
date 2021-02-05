049661c8aaa65a577221546a5698c82234f06f0b
qingqingqingli <kadlqbetty@gmail.com>
2021-02-05 11:51:01 +0100

    update tester

--- Makefile
+++
@@ -1,3 +0,0 @@
-all:
-	nasm -f elf64 -o ft_strlen.o ft_strlen.asm
-	ld -o ft_strlen ft_strlen.o
--- libasm/ft_strdup.asm
+++ libasm/ft_strdup.asm
@@ -43,7 +43,7 @@ _copy_string:
 	ret						; return dst in rax
 
 _error:
-	call	__errno_location	; call the function
-	mov		[rax], 12		; 12 stands for ENOMEM
+	call	__errno_location	; call the
+	; mov		[rax], byte[12]		; 12 stands for ENOMEM
 	mov		rax, 0
 	ret
